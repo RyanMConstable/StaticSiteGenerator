264b3958c632c7efb831236e46725991b82a7b37
RyanMConstable <RyanMConstable>
2025-01-18 19:36:52 -0500

    Added fully fleshed out tests for text to html

--- src/test_nodetohtml.py
+++ src/test_nodetohtml.py
@@ -9,3 +9,23 @@ class TestNodetoHTML(unittest.TestCase):
     def test_text(self):
         text_node = TextNode("Testing", TextType.NORMAL)
         self.assertEqual(f'{text_node_to_html_node(text_node)}', 'HTMLNode(None, Testing, None, None)')
+
+    def test_bold(self):
+        text_node = TextNode("Testing", TextType.BOLD)
+        self.assertEqual(f'{text_node_to_html_node(text_node)}', 'HTMLNode(b, Testing, None, None)')
+
+    def test_italic(self):
+        text_node = TextNode("Testing", TextType.ITALIC)
+        self.assertEqual(f'{text_node_to_html_node(text_node)}', 'HTMLNode(i, Testing, None, None)')
+
+    def test_code(self):
+        text_node = TextNode("Testing", TextType.CODE)
+        self.assertEqual(f'{text_node_to_html_node(text_node)}', 'HTMLNode(code, Testing, None, None)')
+
+    def test_links(self):
+        text_node = TextNode("Testing", TextType.LINKS, "https://www.google.com")
+        self.assertEqual(f'{text_node_to_html_node(text_node)}', "HTMLNode(a, Testing, None, {'href': 'https://www.google.com'})")
+
+    def test_images(self):
+        text_node = TextNode("Testing", TextType.IMAGES, "https://test")
+        self.assertEqual(f'{text_node_to_html_node(text_node)}', "HTMLNode(img, None, None, {'src': 'https://test', 'alt': 'Testing'})")
